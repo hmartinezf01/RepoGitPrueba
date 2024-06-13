9825494cd9fcf4447c72f7a830d0adac00cbb0d5
hmartinezf01 <hmartinezf01@educarex.es>
2024-06-13 13:26:37 +0200

    añadida clase vector nombres y modificacion en usoarray

--- UsoArrays.java
+++ UsoArrays.java
@@ -1,10 +1,27 @@
 package Clases;
-
+import javax.swing.JOptionPane;
 public class UsoArrays {
-	
-public static void main(String[] args) {
-		Arrays miArray=new Arrays(5);
+ public static void main(String[] args) {
+ // TODO Esbozo de método generado automáticamente
+ int opcion=Integer.parseInt(JOptionPane.showInputDialog("Dame una opción (1-2)"));
+ if(opcion==1) {
+ Arrays miArray=new Arrays(5);
+miArray.getDatosArrays();
 
-		miArray.getDatosArrays();
- 	}
-}
+ }else {
+if(opcion==2) {
+
+ int cont=Integer.parseInt(JOptionPane.showInputDialog("¿Cuántos nombres quieres introducir?"));
+ VectorNombres vNombre=new VectorNombres(cont);
+
+ for (int i=0;i<cont;i++) {
+ String nom=JOptionPane.showInputDialog("Dame un nombre:");
+ vNombre.setGuardarNombre(nom);
+ }
+
+ vNombre.getNombres();
+ }
+ }
+ }
+} 
+
